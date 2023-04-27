fd4c5dd58cf4d0a9af635447638552749960dbdb
Anton Österberg <anton.osterberg@protonmail.com>
2023-04-27 12:00:40 +0200

    Delete redundant file

--- lib/src/main/kotlin/io/nexure/fsm/Coroutines.kt
+++
@@ -1,6 +0,0 @@
-package io.nexure.fsm
-
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.runBlocking
-
-internal fun <T> Deferred<T>.awaitBlocking() = runBlocking { this@awaitBlocking.await() }
